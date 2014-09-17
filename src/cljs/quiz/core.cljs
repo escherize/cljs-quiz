6d63249509d2644d4e01589715421bed88d26d13
Alexandre Karpov <alexakarpov@gmail.com>
2014-09-17 12:41:12 -0700

    conclusive remarks

--- src/cljs/quiz/core.cljs
+++ src/cljs/quiz/core.cljs
@@ -36,11 +36,12 @@
 
 (defn end-message [score]
   (cond
-   (= score 0) "0/18: Wow, go signup for a job at amazon.com"
-   (<= 1 score 5) "1"
-   (<= 6 score 10) "2"
-   (<= 11 score 18) "3"
-   :else "you da man!"))
+   (= score 0) "Either you know all the right answers, and avoided them on purpose, or you've managed to miss them all - which is an astronomically low probability. You should put that to use playing lottery!"
+   (<= 1 score 5) "Hm... looks like you've picked up some quotes from people who read the book... why not join their ranks? It's really worth reading!"
+   (<= 6 score 10) "Hm... looks like you've read the first book... may I suggest you keep on reading? You won't be disappointed, I swear!"
+   (<= 11 score 17) "Hey, looks like we've got us a real hitchhiker! Pleasure to meet you. I bet you even know what a Hooloovoo is!"
+   (= 18 score) "I knew it! Douglas Adams isn't dead - he's alive, and is attending the Strange Loop! We salute you, sir! It's an honor to welcome you in our booth."
+   :else "Wait a minute... how did you get here? You're not supposed to be here! Who do you think you are, Ford Prefect or something?"))
 
 (defn tally-score []
   (let [checked-buttons (->> ["input:checked"] sel)
